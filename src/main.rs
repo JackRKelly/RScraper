4cba403f5699633e13fc6f64fae718f2481d7c6e
MyManVan <kcjackkelly@gmail.com>
2020-04-16 11:20:43 -0500

    Added a user input field

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input link to be scraped.");
+
+    let mut link = String::new();
+
+    io::stdin().read_line(&mut link)
+    .expect("Failed to read line");
+
+
+    println!("Scraping: {}", link)
 }
