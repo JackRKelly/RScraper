0c72b89fff10dc329af84d03563716f2c5dfec34
MyManVan <kcjackkelly@gmail.com>
2020-04-16 16:20:02 -0500

    Added user input link

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ use std::io;
 use reqwest;
 
 fn main() -> reqwest::Result<()> {
-    let body = reqwest::blocking::get("https://j2.business")?
+    println!("Please input your link.");
+
+    let mut link = String::new();
+
+    io::stdin().read_line(&mut link)
+        .expect("Failed to read line");
+
+    let body = reqwest::blocking::get(&link)?
       .text()?;
     println!("body = {:?}", body);
     Ok(())
-  }
+}
