8383648969a74f0da7ba5027d2710c94147816d6
MyManVan <kcjackkelly@gmail.com>
2020-04-16 23:43:19 -0500

    Implemented Pretty Table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use prettytable::{Table, Row, Cell};
 use regex::Regex;
 use reqwest;
 use std::io;
@@ -12,20 +13,22 @@ fn main() -> reqwest::Result<()> {
         .expect("Failed to read line");
 
     let body = reqwest::blocking::get(&link)?.text()?;
-    //println!("body = {:?}", body);
-
     let link_regex = Regex::new(r#"href=(?:"([^"]+)"|'(?:'([^']+)'))"#).unwrap();
+    let mut table = Table::new();
+    
+    table.add_row(Row::new(vec![Cell::new(&format!("Scrape results for: {}", link))]));
 
     for links in link_regex.captures_iter(&body) {
         let current_link = links.get(1).unwrap().as_str();
         if current_link.starts_with("http://") || current_link.starts_with("https://") {
-            println!("{}", current_link);
+            table.add_row(Row::new(vec![Cell::new(current_link)]));
         } else if current_link.starts_with("#") {
 
         } else {
-            println!("{}/{}", link.trim(), current_link);
-        }
+            table.add_row(Row::new(vec![Cell::new(&format!("{}/{}", link.trim(), current_link))]));
+        };
     }
+    table.printstd();
 
     Ok(())
 }
