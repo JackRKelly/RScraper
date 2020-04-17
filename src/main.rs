80d313f48b49fdd91119502610dba27a4ac33ac4
MyManVan <kcjackkelly@gmail.com>
2020-04-16 22:48:38 -0500

    Added link for local resources

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-use std::io;
-use reqwest;
 use regex::Regex;
+use reqwest;
+use std::io;
 
 fn main() -> reqwest::Result<()> {
     println!("Please input your link.");
 
     let mut link = String::new();
 
-    io::stdin().read_line(&mut link)
+    io::stdin()
+        .read_line(&mut link)
         .expect("Failed to read line");
 
-    let body = reqwest::blocking::get(&link)?
-      .text()?;
+    let body = reqwest::blocking::get(&link)?.text()?;
     //println!("body = {:?}", body);
 
     let link_regex = Regex::new(r#"href=(?:"([^"]+)"|'(?:'([^']+)'))"#).unwrap();
 
-    for link in link_regex.captures_iter(&body) {
-        println!("{:?}", link.get(1).unwrap().as_str());
+    for links in link_regex.captures_iter(&body) {
+        let current_link = links.get(1).unwrap().as_str();
+        if current_link.starts_with("http://") || current_link.starts_with("https://") {
+            println!("{:?}", current_link);
+        } else {
+            println!("{}/{:?}", link, current_link);
+        }
     }
-
 
     Ok(())
 }
