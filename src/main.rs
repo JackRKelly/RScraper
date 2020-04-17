3c80fc990b23395ff819eed594b31fe6e1848fed
MyManVan <kcjackkelly@gmail.com>
2020-04-17 00:17:03 -0500

    Removed pretty table rs.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use prettytable::{Table, Row, Cell};
 use regex::Regex;
 use reqwest;
 use std::io;
@@ -7,6 +6,7 @@ fn main() -> reqwest::Result<()> {
     println!("Please input your link.");
 
     let mut link = String::new();
+    let mut link_list: Vec<String> = Vec::new();
 
     io::stdin()
         .read_line(&mut link)
@@ -14,21 +14,20 @@ fn main() -> reqwest::Result<()> {
 
     let body = reqwest::blocking::get(&link)?.text()?;
     let link_regex = Regex::new(r#"href=(?:"([^"]+)"|'(?:'([^']+)'))"#).unwrap();
-    let mut table = Table::new();
-    
-    table.add_row(Row::new(vec![Cell::new(&format!("Scrape results for: {}", link))]));
 
     for links in link_regex.captures_iter(&body) {
         let current_link = links.get(1).unwrap().as_str();
         if current_link.starts_with("http://") || current_link.starts_with("https://") {
-            table.add_row(Row::new(vec![Cell::new(current_link)]));
+            link_list.push(current_link.to_string());
         } else if current_link.starts_with("#") {
-
         } else {
-            table.add_row(Row::new(vec![Cell::new(&format!("{}/{}", link.trim(), current_link))]));
+            link_list.push(format!("{}/{}", link.trim(), current_link));
         };
     }
-    table.printstd();
+
+    let sorted = link_list.iter().map(|s| s.len()).max();
+
+    println!("{:?}", sorted);
 
     Ok(())
 }
